a33c56384213dc2f1038ab02514025ec758a140e
hang-1n-there <s22037@gsm.hs.kr>
2024-04-03 10:52:44 +0900

    ✨ seq2seq encoder

--- model/seq2seq.py
+++ model/seq2seq.py
@@ -35,3 +35,35 @@ class Attention(nn.Module):
 
         return context_vector
     
+class Encoder(nn.Module):
+
+    def __init__(self, word_vec_size, hidden_size, n_layers=4, dropout_p=.2):
+        super(Encoder, self).__init__()
+
+        self.rnn = nn.LSTM(
+            word_vec_size,
+            int(hidden_size / 2),
+            num_layers=n_layers,
+            dropout=dropout_p,
+            bidirectional=True,
+            batch_first=True,
+        )
+
+    def forward(self, emb):
+        # |emb| = (batch_size, length, word_vec_size)
+
+        if isinstance(emb, tuple):
+            x, lengths = emb
+            x = pack(x, lengths.tolist(), batch_first=True)
+
+        else:
+            x = emb
+
+        y, h = self.rnn(x)
+        # |y| = (batch_size, length, hidden_size)
+        # |h[0]| = (num_layers * 2, batch_size, hidden_size / 2)
+
+        if isinstance(emb, tuple):
+            y, _ = unpack(y, batch_first=True)
+
+        return y, h
